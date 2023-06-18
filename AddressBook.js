c79d00e4a203e57ee2bb4c25e781d5ae695bb1d5
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-18 14:43:56 +0530

    Added : code to create an address book

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,32 @@
-console.log("Welcome to address Book System using Java Script");
+class AddressBookMain{
+    // Property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    // constructor
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // toString method
+    toString(){
+        return "First Name = " + this.firstName + ", Last Name = " + this.lastName + ", Address = " + this.address +
+                ", City = " + this.city + ", State = " + this.state + ", Zip = " + this.zip + ", Phone Number = " +
+                this.phoneNumber + ", Email = " + this.email
+    }
+}
+let addressBookMain = new AddressBookMain("Rahul", "Kumar", "Banka", "Bhagalpur", "Bihar", 813211, 9934691893, "rahul@123");
+console.log(addressBookMain);
